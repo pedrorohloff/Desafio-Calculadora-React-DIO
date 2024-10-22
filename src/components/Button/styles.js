a35290f3a090dfb20486ba92efee9c4048f5114c
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2024-10-22 11:08:02 -0300

    Initial Commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 30px;
+    border: 1px solid #cdcdcd;
+    background-color: #00AAF0;
+    color: #FFFFFF;
+    font-size: 24;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
